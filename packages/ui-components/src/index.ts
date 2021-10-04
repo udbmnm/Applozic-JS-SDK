65a9acca58ac48297909cd56b6c46b196220c1be
Abhilash Mirji <abhilashmirji@Abhilashs-MacBook-Pro.local>
2021-10-04 19:12:30 +0530

    story migration

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -6,8 +6,10 @@ import FullView from "./views/FullView";
 
 import { useColorMode } from "@chakra-ui/react";
 import Feature from "./models/Feature";
+import Button from "./components/Button";
+import { ButtonProps } from "./components/Button/Button";
 
-export { FullView, MessageStatus, Feature, useColorMode };
+export { Button, FullView, MessageStatus, Feature, useColorMode };
 
 // Models
-export type { Message };
+export type { Message, ButtonProps };
