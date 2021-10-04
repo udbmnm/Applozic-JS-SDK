4c7671bf9e0c47e87c925258ee7375f69088790d
Abhilash Mirji <abhilashmirji@Abhilashs-MacBook-Pro.local>
2021-10-04 20:08:30 +0530

    tsconfig changes

--- packages/ui-storybook/.storybook/main.js
+++ packages/ui-storybook/.storybook/main.js
@@ -2,7 +2,7 @@ const path = require("path");
 const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
-  stories: ["../src/**/*.stories.tsx"],
+  stories: ["../stories/*.stories.tsx"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   webpackFinal: async (config) => {
     return {
--- packages/ui-storybook/stories/Button.stories.tsx
+++ packages/ui-storybook/stories/Button.stories.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Meta } from "@storybook/react/types-6-0";
 import { Story } from "@storybook/react";
-import Button, { ButtonProps } from "@applozic/ui-components";
-
+import { Button, ButtonProps } from "@applozic/ui-components";
 export default {
   title: "Core/Button",
   component: Button,
